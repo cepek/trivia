c998f01c7d4b1442908facb029bd351bdb7a6f2c
Ales Cepek <cepek@gnu.org>
2020-03-18 13:36:12 +0100

    bash script for creating qt project zip archives

---
+++ bash/qtproject-zip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Create a zip archive of the qt project directory with all *.user*
+# files removed
+#
+
+if [ $# -ne 1 ];
+then
+    FULLFILE=$0
+    FILE="$(basename $FULLFILE)"
+    echo "usage:  ${FILE}  qtproject_directory"
+    exit 0
+fi
+
+DIRNAME=$(echo $1 | sed 's:/*$::')
+zip ${DIRNAME}.zip -x '*.user*'  -r ${DIRNAME}
